531390afbd75d8d2044d0fafb5b69d00dbb4206f
Tamas Tatai <tataitamas@gmail.com>
2017-02-22 20:41:20 +0100

    simple timers

---
+++ timers.js
@@ -0,0 +1,10 @@
+// setTimeout
+
+setTimeout(() => { console.log('stopped (3 secs)'); }, 3000);
+console.log('start');
+
+// setInterval
+
+setInterval(() => { console.log('0.2 sec interval'); }, 200);
+console.log("start");
+
