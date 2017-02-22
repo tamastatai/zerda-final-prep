7b2223222896ae3b67b3294a4ffcdc99a340fba1
Tamas Tatai <tataitamas@gmail.com>
2017-02-22 19:50:55 +0100

    higher order functions

--- higher-order.js
+++ higher-order.js
@@ -7,11 +7,49 @@ let arr = [
   [8, 9, 10, 11, 12]
 ]
 
-
 let arr2 = arr.map(function (e) {
   return [ e.reduce(function (a, b) {
     return a + b;
   }, 0) ];
 })
 console.log(arr2);
+
+// foreach
+
+var ramones = ['Joey', 'Johnny', 'Dee Dee', 'Tommy'];
+var ramonesFull = ramones.forEach((e) => {
+  console.log(e + " Ramone");
+});
+
+// filter
+
+// const numbers = [11, 666, 12, 42, 88, 220, -99];
+// const evens = numbers.filter((e) => {
+//   return e % 2 === 0;
+// });
+// console.log(evens);
+
+// map
+
+// var numbers = [11, 666, 12, 42, 88, 220, -99];
+// const tenTimes = numbers.map((e) =>{
+//   return e * 10;
+// });
+// console.log(tenTimes);
+
+// some
+
+// const anyEven = [11, 3, 1, 55, 7].some((e) => {
+//   return e % 2 === 0;
+// });
+// console.log(isAnyEven); // false
+
+// // every
+
+// const allEven = [4, 666, 20, 88, 22].every((e) => {
+//   return e % 2 === 0;
+// });
+// console.log(isAllEven); // true
+
+
 
